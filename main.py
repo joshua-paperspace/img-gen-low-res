f6353d8866325ad1a89ac9c2614f532457b138b5
paperspace <joshua@paperspace.com>
2023-05-24 14:02:27 +0000

    working app link

--- main.py
+++ main.py
@@ -8,18 +8,15 @@ import base64
 from threading import Thread
 import json
 import time
+from config import config
 
 
 app = FastAPI()
 
-
 def call_high_res_api(json_obj):
-    dictToSend = json_obj
-
-    requests.post("http://0.0.0.0:8001/generate",json=dictToSend, headers = {'content-type': 'application/json'})
-
-    return "Done!"
-
+    # Will need to change the url address below
+    requests.post(config.url, data=json_obj)
+    return "Sent call to high res app!"
 
 @app.get("/")
 async def root():
@@ -27,26 +24,17 @@ async def root():
 
 @app.post("/generate")
 async def predict(prompt: str):
-    # image = pipe(prompt).images[0]
-
-    # print(type(init_image))
-
-    # image = pipe(prompt=prompt, image=init_image, strength=0.75, guidance_scale=7.5).images[0]
     image = pipe(prompt).images[0] 
+    print(f"Image size: {image.size}")
     image.save("low_res.png")
 
     rawBytes = io.BytesIO()
-    image.save(rawBytes, "png")
+    image.save(rawBytes, "PNG")
     rawBytes.seek(0)
     img_base64 = base64.b64encode(rawBytes.read())
     
-    json_object = json.dumps({'init_image':str(img_base64), 'prompt':str(prompt)})
+    json_object = json.dumps({'init_image': str(img_base64), 'prompt': str(prompt)})
     Thread(target = call_high_res_api, args=(json_object,)).start()
     time.sleep(3)
-    
-    # return jsonify({'userID': userID, 'generated_img':str(img_base64)})
 
     return FileResponse('low_res.png', media_type="image/jpeg")
-
-
-    # images = pipe(prompt=prompt, image=init_image, strength=0.75, guidance_scale=7.5).images
